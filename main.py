2f176c8ebb95c525f1c128fc42427c84d2aba90c
Xin Sun <115094595+Windft@users.noreply.github.com>
2023-03-29 16:23:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QPropertyAnimation
-from PyQt5.QtWidgets import QLabel, QMessageBox
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLabel, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5.QtCore import Qt
+import pandas as pd
+import json
 
 pyQTfileName = "test_ui.ui"
 
@@ -14,7 +18,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.menu_button.clicked.connect(lambda: self.toggle_menu(250, True))
-        self.Btn_1.clicked.connect(lambda: self.navigate_to_view_all('Display All Vulnerabilities with \n Description and CVE link, May be add\n CVSS score too'))
+        self.Btn_1.clicked.connect(lambda: self.navigate_to_view_all('Display All Vulnerabilities with \nDescription and CVE link, May be add\nCVSS score too'))
         self.Btn_2.clicked.connect(lambda: self.navigate_to_check('Check for Vulnerabilities'))
         self.Btn_3.clicked.connect(lambda: self.navigate_to_fix('Available Fixes'))
         self.Btn_4.clicked.connect(lambda: self.navigate_to_add_new('Add New Vulnerability'))
@@ -46,7 +50,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def navigate_to_view_all(self, msg):
         self.Page_widgets.setCurrentWidget(self.page_2)
         self.page_2_label.setText(msg)
-        
+
         # Import data
         with open('test_data.json', 'r') as f:
             data = json.load(f)
@@ -64,14 +68,13 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         font = self.Table_Cve.horizontalHeader().font()
         font.setBold(True)
         self.Table_Cve.horizontalHeader().setFont(font)
-        
         # Add data into the table
         for i in range(len(cve)):
             for j in range(len(cve.columns)):
                 item = QTableWidgetItem(str(cve.iloc[i, j]))
                 item.setTextAlignment(Qt.AlignCenter)
                 self.Table_Cve.setItem(i, j, item)
-        
+
 
     def navigate_to_check(self, msg):
         self.Page_widgets.setCurrentWidget(self.page_3)
@@ -85,8 +88,42 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.Page_widgets.setCurrentWidget(self.page_5)
         self.page_5_label.setText(msg)
 
+        # Button action
+        self.btn_cve_add.clicked.connect(self.Cve_Add)
+
+
+
+    def Cve_Add(self):
+        # Get attributes fo new cve
+        NewName = self.cve_name.toPlainText()
+        NewDesc = self.cve_description.toPlainText()
+        NewCVSS = float(self.cve_cvss.toPlainText())
+        NewLink = self.cve_link.toPlainText()
+        NewScript = self.cve_script.toPlainText()
+
+        # Store as dict
+        NewDataDict = dict(CVE=NewName,
+                    Description=NewDesc,
+                    CVSS=NewCVSS,
+                    Link=NewLink,
+                    Script=NewScript)
+
+        # update json file
+        with open('test_data.json', 'r') as f:
+            OldData = json.load(f)
+        if len(OldData) > 1:
+            OldData.append(NewDataDict)
+        else:
+            OldData = list(OldData)
+        with open('test_data.json', 'w') as f_new:
+            json.dump(OldData, f_new)
+
+        self.outstate.setPlainText('Success!')
+
+
+
     def fix_vulnerability(self):
-        msgbox = QMessageBox();
+        msgbox = QMessageBox()
         msg =''
         if self.radioButton_1.isChecked():
             msg = self.radioButton_1.text()
